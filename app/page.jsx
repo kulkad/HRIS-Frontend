980aed49e9fe35aacc6712be860355b1418b09e1
ryunex15 <badarudin1511@gmail.com>
2024-07-10 09:56:17 +0700

    a

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <div className='bg-white dark:bg-slate-900 dark:text-white rounded-lg mx-4 p-4'>
-      <h1 className='text-2xl font-semibold'>Selamat Datang di HRIS-CORPS</h1>
+      <h1 className='text-2xl font-semibold'>Selamat Datang, users</h1>
       <hr className='mt-5 mb-5'/>
 <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Absen Hari Ini</button>
     </div>
