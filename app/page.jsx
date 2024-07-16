aeca1353ecfa3bc43963ea73a14716686b1cf3df
abyanyusufd <abyannn11111@gmail.com>
2024-07-16 13:02:03 +0700

    slide profil

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,9 @@
 "use client";
 
-import React from "react";
-import { useEffect, useState } from "react";
-//import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
 const HomePage = () => {
-  // Pe
   const [user, setUser] = useState(null);
-  // const router = useRouter();
 
   useEffect(() => {
     const userData = localStorage.getItem("user");
@@ -21,18 +17,27 @@ const HomePage = () => {
   if (!user) return <p>Loading...</p>;
 
   return (
-    <div className='bg-white dark:bg-slate-900 dark:text-white rounded-lg mx-4 p-4'>
-      <h1 className='text-2xl font-semibold'>Selamat Datang, {user.name}</h1>
-      <hr className='mt-5 mb-5'/>
+    <div className="bg-white dark:bg-slate-900 dark:text-white rounded-lg mx-4 p-4">
+      <h1 className="text-2xl font-semibold">Selamat Datang, {user.name}</h1>
+      <hr className="mt-5 mb-5" />
       <a href="geolocation">
-      <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Geolocation</button>
+        <button
+          type="button"
+          className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+        >
+          Geolocation
+        </button>
       </a>
       <a href="halaman-absen">
-<button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Absen Hari Ini</button>
+        <button
+          type="button"
+          className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+        >
+          Absen Hari Ini
+        </button>
       </a>
     </div>
   );
 };
-
 
 export default HomePage;
