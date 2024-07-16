2538f8f40332d2a1832fbae10e946d69309fb7ec
ryunex15 <badarudin1511@gmail.com>
2024-07-16 10:04:30 +0700

    perbarui

--- app/page.jsx
+++ app/page.jsx
@@ -24,6 +24,9 @@ const HomePage = () => {
     <div className='bg-white dark:bg-slate-900 dark:text-white rounded-lg mx-4 p-4'>
       <h1 className='text-2xl font-semibold'>Selamat Datang, {user.name}</h1>
       <hr className='mt-5 mb-5'/>
+      <a href="geolocation">
+      <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Geolocation</button>
+      </a>
       <a href="halaman-absen">
 <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Absen Hari Ini</button>
       </a>
