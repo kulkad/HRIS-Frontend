961277bae71678b8c8a9f260209e247363d9f5a8
kulkad <rdarmawan281@gmail.com>
2024-07-16 15:21:50 +0700

    memperbarui tampilan detail user

--- app/detailuser/page.jsx
+++ app/detailuser/page.jsx
@@ -3,15 +3,19 @@
 import React from "react";
 import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
+import { IoIosArrowBack } from "react-icons/io";
 import { SlOptionsVertical } from "react-icons/sl";
 
 const DetailUser = () => {
   return (
     <div className="bg-white rounded-lg mx-4 p-4 text-xl">
-      <p className="px-6 py-10 font-semibold">DETAIL USER</p>
+      
 
       {/* Tampilan Untuk Laptop */}
+      
       <div className="relative overflow-x-auto hidden sm:block">
+      <p className="px-6 py-10 font-semibold">DETAIL USER</p>
         <table className="w-full text-sm text-left rtl:text-right">
           <thead className="text-xs text-gray-900 uppercase dark:text-gray-400">
             <tr>
@@ -47,7 +51,7 @@ const DetailUser = () => {
               >
                 Jahdan Kopling
               </td>
-              <td className="px-6 py-4 font-medium text-gray-900">PKL</td>
+              <td className="px-6 py-4 font-medium text-gray-900 ">PKL</td>
               <td className="px-6 py-4 font-medium text-gray-900">
                 jahdankopling@gmail.com
               </td>
@@ -59,14 +63,36 @@ const DetailUser = () => {
 
       {/* Tampilan Untuk Hp */}
 
-      <div class="relative overflow-x-auto w-full flex sm:hidden">
-        <div class=" p-4 leading-normal">
-          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Ang Badarudin
+      <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 sm:hidden">
+        <div class="flex justify-start px-4 pt-4">
+        <Link href="/profile">
+            <li className="flex items-center py-3">
+              <IoIosArrowBack className="mr-1"/>
+            </li>
+          </Link>
+        </div>
+        <div class="flex flex-col items-center pb-10">
+          <img
+            class="w-24 h-24 mb-3 rounded-full shadow-lg"
+            src="/assets/images/windah.jpg"
+            alt="Bonnie image"
+          />
+          <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+            Jahdan Kopling
           </h5>
-          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            angbadarudin@gmail.com
-          </p>
+          <span class="text-lg text-gray-500 dark:text-gray-400">
+            PKL
+          </span>
+          <div class="mt-4 md:mt-6">
+            <div className="mb-5">
+            <label className="font-semibold text-sm">Email  : </label>
+            <p className="font-semibold">Jahdankopling@gmail.com</p>
+            </div>
+            <div> 
+            <label className="font-semibold text-sm">Gender  :</label>
+            <p className="font-semibold">Laki-laki</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
