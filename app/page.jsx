d8b5f98762e294624bcd1d5a6d90b606f02f2228
kulkad <rdarmawan281@gmail.com>
2024-07-18 09:06:52 +0700

    memperbarui card di dashboard

--- app/page.jsx
+++ app/page.jsx
@@ -42,14 +42,27 @@ const HomePage = () => {
             src="/assets/images/windah.jpg"
             alt=""
           />
-          <div class="flex flex-col justify-between p-4 leading-normal">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
+          <div class="flex flex-col justify-between p-5 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white ml">
+             {user.name}
             </h5>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
+             Jam Masuk :
             </p>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+             Jam Pulang :
+            </p>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+             Estimasi Denda : 
+            </p>
+            <a href="geolocation">
+          <button
+            type="button"
+            className="focus:outline-none text-white bg-red-500 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+          >
+            Total Denda Keterlambatan
+          </button>
+        </a>
           </div>
         </div>
 
