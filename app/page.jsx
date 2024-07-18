c5d1ef1c08330e81f9701924c03bf688e6bd799e
kulkad <rdarmawan281@gmail.com>
2024-07-18 11:46:40 +0700

    test

--- app/page.jsx
+++ app/page.jsx
@@ -38,7 +38,7 @@ const HomePage = () => {
           class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
         >
           <img
-            class="object-cover w-full rounded-lg h-96 md:h-auto md:w-48 md:rounded-lg ml-3"
+            class="object-cover w-full rounded-lg h-96 md:h-auto md:w-48 md:rounded-lg ml-1 "
             src="/assets/images/windah.jpg"
             alt=""
           />
